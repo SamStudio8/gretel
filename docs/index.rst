9310b827695967522bbe5bf7b7fdb1cc159020ec
Sam Nicholls <sam.n@studio8media.co.uk>
2016-06-13 13:47:36 +0100

    first commit

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. Gretel documentation master file, created by
+   sphinx-quickstart on Mon Jun 13 13:40:08 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Gretel's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+requirements = [
+    "numpy",
+]
+
+test_requirements = [
+
+]
+
+setuptools.setup(
+    name="gretel",
+    version="0.0.1a",
+    url="https://github.com/samstudio8/gretel",
+
+    description="",
+    long_description="",
+
+    author="Sam Nicholls",
+    author_email="sam@samnicholls.net",
+
+    maintainer="Sam Nicholls",
+    maintainer_email="sam@samnicholls.net",
+
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+
+    install_requires=requirements,
+
+    entry_points = {
+    },
+
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    test_suite="tests",
+    tests_require=test_requirements
+)
