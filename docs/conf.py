b7089f6dd2a4c740112472fbea6404221a06b370
Sam Nicholls <sam.n@studio8media.co.uk>
2016-08-18 23:17:41 +0100

    Sphinx mocks

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,11 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
+import mock
+MOCK_MODULES = ['numpy', 'matplotlib', 'pysam', 'vcf', 'hansel']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
